4dc1f0f4527cc3433fc54c017f447db065657e8e
JaskaranSingh24 <130111903+JaskaranSingh24@users.noreply.github.com>
2025-03-04 17:31:01 -0800

    Create Home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the T-Shirt Store!</h1>
+      <p>Your one-stop shop for awesome T-shirts.</p>
+      <a href="/products">Browse Products</a>
+    </div>
+  );
+}
+
+export default Home;
